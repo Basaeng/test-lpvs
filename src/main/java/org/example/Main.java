2d5c9c2c71b4467ba7c07cd24525f35cacef9b02
Basaeng <rjsqktod1@gmail.com>
2023-05-20 20:33:52 +0900

    test2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I can show you the world!");
+        System.out.println("under the sea!");
     }
 }
